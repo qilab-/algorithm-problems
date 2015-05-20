ca3b2b67dad2ebdb20361c4c9c07160063b856d9
qilab <qilab@gmx.com>
2015-05-21 01:21:34 +0900

    DRY

--- P/S007_dataHistogram/Main.java
+++ P/S007_dataHistogram/Main.java
@@ -5,60 +5,55 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class Main {
+public class Main_2 {
   private static final String SPACE = "\u0020";
-  
+  private static final String LEFT_BRACKET = "(";
+  private static final String RIGHT_BRACKET = ")";
+  private static final char LITTLE_A = 'a';
+
+  private static final Pattern LITTLE = Pattern.compile("^[a-z]$");
+  private static final Pattern NUMBER = Pattern.compile("^[0-9]$");
+
   public static void main(String args[] ) throws IOException {
     String afterCompression = null;
-    
-    // input from console
+
     try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
       afterCompression = br.readLine();
     }
-    
-    final Pattern LITTLE = Pattern.compile("^[a-z]$");
-    final Pattern NUMBER = Pattern.compile("^[0-9]$");
-    final char RIGHT_BRACKET = ')';
-    final char LITTLE_A = 'a';
-    
+
     long[] result = new long[26];
-    List<Integer> numList = new ArrayList<>();
-    
+    List<Long> numList = new ArrayList<>();
+
+    String number = "";
     for (int i = 0; i < afterCompression.length(); i++) {
-      char ch = afterCompression.charAt(i);
-      String str = afterCompression.substring(i, i + 1);
-      if (LITTLE.matcher(str).matches()) {
-        long n = 1;
-        for (int j : numList) {
+      String c = afterCompression.substring(i, i + 1);
+      if (LITTLE.matcher(c).matches()) {
+        long n = 1L;
+        if (!number.isEmpty()) {
+          n = Long.parseLong(number);
+          number = "";
+        }
+        for (long j : numList) {
           n *= j;
         }
-        result[ch - LITTLE_A] += n;
-      } else if (NUMBER.matcher(str).matches()) {
-        String number = str;
-        String next = afterCompression.substring(i + 1, i + 2);
-        while (NUMBER.matcher(next).matches()) {
-          number += next;
-          i++;
-          next = afterCompression.substring(i + 1, i + 2);
+        result[c.charAt(0) - LITTLE_A] += n;
+      } else if (NUMBER.matcher(c).matches()) {
+        number += c;
+      } else if (c.equals(LEFT_BRACKET)) {
+        long n = 1L;
+        if (!number.isEmpty()) {
+          n = Long.parseLong(number);
+          number = "";
         }
-        if (next.equals("(")) {
-          numList.add(Integer.valueOf(number));
-        } else if (LITTLE.matcher(next).matches()) {
-          long n = Integer.parseInt(number);
-          for (Integer j : numList) {
-            n *= j;
-          }
-          result[next.charAt(0) - LITTLE_A] += n;
-          i++;
-        }
-      } else if (ch == RIGHT_BRACKET) {
+        numList.add(n);
+      } else if (c.equals(RIGHT_BRACKET)) {
         numList.remove(numList.size() - 1);
       }
     }
-    
+
     for (int i = 0; i < 26; i++) {
       System.out.println((char)(i + LITTLE_A) + SPACE + result[i]);
     }
   }
-  
+
 }
